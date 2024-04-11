d2f61bf89e3f51beaee3ed65a19806802779abec
GDuranHenriquez <gregorioduranedrian123@gmail.com>
2024-04-11 13:24:24 -0400

    GDuran pdf puppeter

--- src/utils/generatePdfCutClose.js
+++ src/utils/generatePdfCutClose.js
@@ -1,24 +1,8 @@
-/* const pdf = require('html-pdf') */
 const puppeteer = require('puppeteer');
 
 const generatePdfCutClose = async (html) => {
   try {
-    /* const options = { 
-      format: 'Letter', 
-      orientation: "portrait", 
-      border: "1cm"
-    };
-    return new Promise((resolve, reject) => {
-      pdf.create(html, options).toBuffer(function(err, buffer){
-        if (err) {
-          console.error('Error al crear el PDF:', err);
-          reject({ error: 'Error al crear el PDF:' });
-        }      
-        const base64String = buffer.toString('base64');
-        resolve(base64String);
-      })
-    }) */  
-
+    
     const browser = await puppeteer.launch({
       headless: true,
       defaultViewport:{
@@ -45,7 +29,7 @@ const generatePdfCutClose = async (html) => {
     return base64String;
     
   } catch (error) {
-    return res.status(400).json({error: error})
+    throw new Error('Error al generar pdf');
   }
 }
 
