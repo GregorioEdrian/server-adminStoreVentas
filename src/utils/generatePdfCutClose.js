ff129def647a507470919bcd33aa68120a9920e4
GDuranHenriquez <gregorioduranedrian123@gmail.com>
2024-04-11 13:47:09 -0400

    GDuran pdf puppeter

--- src/utils/generatePdfCutClose.js
+++ src/utils/generatePdfCutClose.js
@@ -2,26 +2,36 @@ const puppeteer = require('puppeteer');
 
 const generatePdfCutClose = async (html) => {
   try {
-      /* 
-        {
-        headless: true,
-        defaultViewport:{
-            width: 816,
-            height: 500,
-            deviceScaleFactor: 1,
-            isMobile: true,
-            hasTouch: false,
-            isLandscape: false
-          }
-        }
-      */
-    const browser = await puppeteer.launch();
+    const pdfOptions = {
+      format: 'A4',
+      printBackground: true,
+      margin: { left: "0cm", top: "1cm", right: "0cm", bottom: "4cm" },
+      /* displayHeaderFooter: true, */
+      /* footerTemplate: footerPdf,
+      headerTemplate: headerPdf, */
+    };
+
+    const browser = await puppeteer.launch({
+      args: [
+        "--disable-web-security",
+      ],
+      defaultViewport: {
+        width: 750,
+        height: 500,
+        deviceScaleFactor: 1,
+        isMobile: false,
+        hasTouch: false,
+        isLandscape: false,
+      }
+    });
+
+    
     const page = await browser.newPage();
-    /* await page.emulateMediaType("screen") */
+    await page.emulateMediaType('screen');
     await page.setContent(html);
     
     // Generar PDF en formato buffer
-    const pdfBuffer = await page.pdf();
+    const pdfBuffer = await page.pdf(pdfOptions);
     
     await browser.close();
     
