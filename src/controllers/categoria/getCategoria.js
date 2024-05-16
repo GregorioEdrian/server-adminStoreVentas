c3df635bd800cfa26ccfb237762011986af92f1e
GDuranHenriquez <gregorioduranedrian123@gmail.com>
2024-05-16 01:52:45 -0400

    Change to Dollar dinamic

--- src/controllers/categoria/getCategoria.js
+++ src/controllers/categoria/getCategoria.js
@@ -1,10 +1,9 @@
-const { Categoria } = require('../../db');
-const changeAllPriceToDollar = require('../../utils/changeAllPrice')
+const { Categoria } = require('../../db')
 
 async function getAllCategorias(req, res){
   try {
     const categorias = await Categoria.findAll();
-    await changeAllPriceToDollar(38)
+    
     return res.status(200).json(categorias);
   } catch (error) {
     return res.status(404).json({error: error.messge});
