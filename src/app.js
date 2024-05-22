1c1def07daa64d8c0993fac352c861415ef35bd8
Gregorio Duran <69368751+GregorioEdrian@users.noreply.github.com>
2024-05-22 11:31:06 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -29,9 +29,14 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use(cors({
-  origin: 'https://admin-ventas-mukafe.netlify.app',
-}));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+server.use(cors());
 
 //server.use('/', routes);
 server.use('/data-user', authenticate, routerDataUser);
